c371a8bf050bac27124b5872fad9fd45fab3abcd
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-18 12:20:56 -0400

    Introduce ScoreRow component

---
+++ src/components/ScoreRow/ScoreRow.js
@@ -0,0 +1,13 @@
+import './ScoreRow.css';
+import Score from '../Score/Score';
+
+function ScoreRow({ score1, score2 }) {
+    return (
+        <div className='Score-row'>
+            <Score label='Score' score={score1} />
+            <Score label='High Score' score={score2} />
+        </div>
+    );
+}
+
+export default ScoreRow;

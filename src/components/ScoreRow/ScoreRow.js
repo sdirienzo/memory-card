37953a761e0b41436d9375ce2644eed427bc7b80
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-20 21:21:21 -0400

    Simplify ScoreRow parent div className

--- src/components/ScoreRow/ScoreRow.js
+++ src/components/ScoreRow/ScoreRow.js
@@ -3,7 +3,7 @@ import Score from '../Score/Score';
 
 function ScoreRow({ score1, score2 }) {
     return (
-        <div className='Score-row'>
+        <div className='ScoreRow'>
             <Score label='Score' score={score1} />
             <Score label='High Score' score={score2} />
         </div>
